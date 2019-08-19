b2daf3b53377f77e7db105d0dd83831b8dee91c6
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-19 14:55:40 +0100

    added new models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews
+  has_many :gardens
+  has_many :bookings
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
+  mount_uploader :photo, PhotoUploader
 end
