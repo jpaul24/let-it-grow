4267a3be79c7aa5a0634e444ed17d644b7b2d6ce
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-20 10:09:09 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :gardens
+  resources :gardens do
+    resources :reviews, only: [:create]
+    resources :bookings, only: [:create, :edit, :update]
+  end
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :bookings, only: [:destroy]
+  resources :reviews, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
