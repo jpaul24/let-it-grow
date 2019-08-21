81dcf5c16446b72150b9191cc7ec6675ff0ba0e4
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-21 12:43:03 +0100

    made the maps and geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_08_21_102002) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +35,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_102002) do
     t.string "purpose"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photos"
+    t.string "photo"
     t.bigint "user_id"
     t.float "latitude"
     t.float "longitude"
