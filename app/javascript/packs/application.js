dd0c4ba3732b9e59624542525ac46eaf30cf8e58
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-21 12:48:12 +0100

    fixed merge issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,10 @@
 import "bootstrap";
-import { mapTool } from "../plugins/map";
 import "../plugins/flatpickr"
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "../plugins/flatpickr"
 
 initMapbox();
-mapTool();
 
 
 
