e16bd85bcacefef6a58cb1c4dff48a8d26d058b2
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-21 12:19:26 +0100

    made the geocode map on the gardens index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_145813) do
+ActiveRecord::Schema.define(version: 2019_08_21_090722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_145813) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
