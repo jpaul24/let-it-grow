92c693c6775d9f2322aa0276925a129566c211fd
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-23 12:22:14 +0100

    added star rating

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,2 +1,11 @@
 import "jquery-bar-rating";
-import "jquery-bar-rating/dist/themes/bootstrap-stars.css";
+import "jquery-bar-rating/dist/themes/css-stars.css";
+// import $ from 'jquery';
+
+const initStarRating = () => {
+  $('#review_rating').barrating({
+    theme: 'css-stars'
+  });
+};
+
+export { initStarRating };
